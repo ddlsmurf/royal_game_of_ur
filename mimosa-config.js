e85742ce018fdf2573778eefae803f8f0a99bc43
Eric Doughty-Papassideris <eric.doughty-papassideris@robosoft.com>
2017-05-13 23:07:08 +0200

    adding bootstrap bower dependency

--- mimosa-config.js
+++ mimosa-config.js
@@ -20,6 +20,14 @@ exports.config = {
     "bowerDir": { "clean": false },
     "copy": {
         "mainOverrides": {
+            "bootstrap": [
+                "docs/assets/js/bootstrap.js",
+                "docs/assets/js/bootstrap.min.js",
+                "docs/assets/css/bootstrap.css",
+                "docs/assets/css/bootstrap-responsive.css",
+                "docs/assets/img/glyphicons-halflings-white.png",
+                "docs/assets/img/glyphicons-halflings.png"
+                ],
             "": []
             }
         }
